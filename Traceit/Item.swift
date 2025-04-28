52e40fa74a8cbdeefe5c3cb5cb4133780ec3735a
dterr0091 <52011919+dterr0091@users.noreply.github.com>
2025-04-28 12:56:51 -0400

    Initial Commit

---
+++ Traceit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Traceit
+//
+//  Created by Dominick Terry on 4/28/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TraceitTests/TraceitTests.swift
@@ -0,0 +1,16 @@
+//
+//  TraceitTests.swift
+//  TraceitTests
+//
+//  Created by Dominick Terry on 4/28/25.
+//
+
+import Testing
+
+struct TraceitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
