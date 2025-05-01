e8525d6f231182442086e8444029d154c0ad57d4
dterr0091 <52011919+dterr0091@users.noreply.github.com>
2025-05-01 08:50:06 -0400

    Remove build artifacts

--- Sources/Models/HomeModel.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct HomeModel: Identifiable, Codable {
-    let id: UUID
-    let title: String
-    let description: String
-} 
--- Sources/Models/User.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct User: Identifiable, Codable {
-    let id: String
-    let email: String?
-    let fullName: String?
-} 
